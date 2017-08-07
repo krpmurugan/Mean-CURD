5c94cdde8c332c59da798754b743de8c0111db1a
Murugan Ramasamy <Murugan Ramasamy@purple.local>
2017-08-07 10:57:43 +0530

    first commit

--- routes/route.js
+++ routes/route.js
@@ -34,26 +34,61 @@ router.post('/contact', function(req,res, next){
     
 });
 
+// Edit data
+router.get('/contact/edit/:id', function(req,res, next){
+     //res.send('Retriving Contact List');
+     Contact.findOne({_id:req.params.id},function(err, contactus){
+         res.json(contactus);
+     })
+});
+
 // Update Contact
 
-router.put('/contact/:id', function(req,res, next){
+/*router.put('/contact/update/:id', function(req, res, next) {
+    Post.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});*/
+
+
+router.put('/contact/update/:id', function(req,res, next){
+
     let newContact = new Contact({
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         phone: req.body.phone
     });
+   
+    Contact.findById(req.params.id, function (err, todo) {  
+    // Handle any possible database errors
+    if (err) {
+        res.status(500).send(err);
+    } else {
+        // Update each attribute with any possible attribute that may have been submitted in the body of the request
+        // If that attribute isn't in the request body, default back to whatever it was before.
+      todo.first_name = req.body.first_name;
+      todo.last_name= req.body.last_name;
+      todo.phone = req.body.phone; 
 
-    newContact.update({_id:req.params.id},function(err, contactus){
-        if(err){
-            res.json({msg: "Failed to Update Contact"});
+      /*todo.first_name = 'James1';
+      todo.last_name= 'Anderson1';
+      todo.phone = '89988998891'; */
+
+
+
+      todo.save(function (err, contactus) {
+        if (err) {
+            res.status(500).send(err)
         }
         else {
-            res.json({msg: "Contact Update successfully."});
+            res.json({msg: "Contact Updated successfully."});
         }
-
+       // res.send('Success' + req.body.first_name);
+      });
+       
+    }
     });
-
-    
 });
 
 // delete contact
